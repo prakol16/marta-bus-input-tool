f189ae45f755590286cbe1abbc5344560cf2fcce
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2017-09-08 22:28:16 -0400

    Removed jekyll

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,3 @@
----
----
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
