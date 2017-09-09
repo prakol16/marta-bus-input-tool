bebac642cf1f3e11ae373dbd99fb534d0c00c9a5
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2017-09-09 00:41:33 -0400

    Changed API key and added marker

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,30 @@
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
 var map, infoWindow;
+function addMarkerAtLocation() {
+	// Try HTML5 geolocation.
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+
+			// infoWindow.setPosition(pos);
+			// infoWindow.setContent('Your Location');
+			new google.maps.Marker({
+				position: pos,
+				map: map
+			});
+		}, function() {
+			handleLocationError(true, infoWindow, map.getCenter());
+		});
+	} else {
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
+	}
+}
+
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -13,41 +37,37 @@ function initMap() {
         zoom: 11,
         disableDefaultUI: true
     });
-    
-	var ctaLayer = new google.maps.KmlLayer({
-		url: 'http://googlemaps.github.io/js-v2-samples/ggeoxml/cta.kml',
-		map: map
-	});
-    // infoWindow = new google.maps.InfoWindow;
 
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            // infoWindow.setPosition(pos);
-            // infoWindow.setContent('Your Location');
-            var marker = new google.maps.Marker({
-                position: pos,
-                map: map
-            })
-
-            // infoWindow.open(map);
-            map.setCenter(pos);
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
     var div = $("#search-menu").clone();
     var searchBox = new google.maps.places.SearchBox(div.find("#search-location")[0]);
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(div[0]);
     div.show();
+    
+	searchBox.addListener('places_changed', function() {
+		var places = searchBox.getPlaces();
+		console.log("Places changed");
+		if (places.length == 0) {
+			return;
+		}
+		places.forEach(function(place) {
+        	if (!place.geometry) {
+				console.log("Returned place contains no geometry");
+				return;
+			}
+
+			// Create a marker for each place.
+			new google.maps.Marker({
+				map: map,
+				title: place.name,
+				position: place.geometry.location
+			});
+		});
+	});
+	
+	div.find("#cur-loc-button").on("click", function() {
+		addMarkerAtLocation();
+		return false;
+	});
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
