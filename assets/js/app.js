5dc828d3a3e6d893458411554e557b0278e4e73b
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2017-09-08 22:26:36 -0400

    Merged Shiv's stuff properly

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,52 @@
 ---
 ---
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+var map, infoWindow;
 
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-	  center: {lat: -34.397, lng: 150.644},
-	  zoom: 8
-	});
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 33.9193213,
+            lng: -84.31686020000001
+        },
+        zoom: 11
+    });
+    // infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            // infoWindow.setPosition(pos);
+            // infoWindow.setContent('Your Location');
+            var marker = new google.maps.Marker({
+                position: pos,
+                map: map
+            })
+
+            // infoWindow.open(map);
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
-$(document).ready(function() {
-	$("#tabs").tabs();
-	console.log("Hi");
-});
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+
+    // infoWindow.setPosition(pos);
+    // infoWindow.setContent(browserHasGeolocation ?
+    //                       'Error: The Geolocation service failed.' :
+    //                       'Error: Your browser doesn\'t support geolocation.');
+    // infoWindow.open(map);
+} // This is just a sample script. Paste your real code (javascript or HTML) here.
